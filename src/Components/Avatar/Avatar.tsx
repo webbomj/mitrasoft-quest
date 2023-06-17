c29cb01c057ca7cc6d2c84100816c1402f723d6d
webbomj <nethack@mail.ru>
2023-06-18 02:43:12 +0300

    feat: add avatar to burger menu

---
+++ src/Components/Avatar/Avatar.tsx
@@ -0,0 +1,9 @@
+import batmanAvatar from "../../Assets/avatar-batman.png";
+
+import "./Avatar.scss";
+
+function Avatar() {
+  return <img className="avatar__img" src={batmanAvatar} />;
+}
+
+export default Avatar;
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,32 +1,26 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+
+import "./Header.scss";
+import Avatar from "../Avatar/Avatar";
 
 function Header() {
   return (
     <header>
-      <Navbar bg="light" expand="lg">
+      <Navbar bg="light" expand="true">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">Posts</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+              <Nav.Link href="#link">Обо мне</Nav.Link>
+              <Nav.Link href="#link">Подробности о пользователе</Nav.Link>
+              <div className="header__info">
+                <Avatar></Avatar>
+                <div className="header__name">Andrey Lapin</div>
+                <div className="header__mail">nethack@mail.ru</div>
+              </div>
             </Nav>
           </Navbar.Collapse>
         </Container>
